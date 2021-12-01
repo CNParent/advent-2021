eaf6eed982f79d3413b68e4c8e8fccd6f7626382
Chris Parent <chrisnparent@gmail.com>
2021-12-01 18:03:50 -0500

    day1

--- day1/day1_1.js
+++ day1/day1_1.js
@@ -1,6 +1,11 @@
 scripts.day1_1 = async () => {
-    await loadScript('');
-
     let data = await getFile('day1/day1_1_input.txt');
-    terminal.textContent = `Enter results here`;
+    let depths = data.split('\n').map(x => Number(x));
+    let increases = 0;
+    for(var i = 1; i < depths.length; i++){
+        if (depths[i] > depths[i-1]) increases++;
+    }
+
+    terminal.textContent = `The depth increased ${increases} times`;
+
 }
--- day1/day1_2.js
+++ day1/day1_2.js
@@ -1,6 +1,12 @@
 scripts.day1_2 = async () => {
-    await loadScript('');
-
     let data = await getFile('day1/day1_1_input.txt');
-    terminal.textContent = `Enter results here`;
+    let depths = data.split('\n').map(x => Number(x));
+    let increases = 0;
+    for(var i = 3; i < depths.length; i++) {
+        var previous = depths[i-3] + depths[i-2] + depths[i-1];
+        var current = depths[i-2] + depths[i-1] + depths[i];
+        if (current > previous) increases++;
+    }
+
+    terminal.textContent = `The depth increased ${increases} times`;
 }
