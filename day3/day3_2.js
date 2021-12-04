e7bc05c5602292cecd401d24a00771b35362f4e2
Chris Parent <chrisnparent@gmail.com>
2021-12-03 23:28:30 -0500

    why not

--- day3/day3_2.js
+++ day3/day3_2.js
@@ -1,40 +1,27 @@
 scripts.day3_2 = async () => {
     let data = await getFile('day3/day3_1_input.txt');
     let numbers = data.split('\r\n');
-    let digits = numbers[0].length;
-    
-    let getRates = (filteredNumbers = numbers) => {
-        let rates = [];
-        for(let i = 0; i < digits; i++) {
-            rates.push({
-                one: filteredNumbers.map(x => x[i] == '1' ? 1 : 0).reduce((a,b) => a + b, 0),
-                zero: filteredNumbers.map(x => x[i] == '0' ? 1 : 0).reduce((a,b) => a + b, 0)
-            });
+
+    let rateAt = (filteredNumbers = numbers, index = 0) => {
+        return {
+            one: filteredNumbers.map(x => x[index] == '1' ? 1 : 0).reduce((a,b) => a + b, 0),
+            zero: filteredNumbers.map(x => x[index] == '0' ? 1 : 0).reduce((a,b) => a + b, 0)
         }
+    }
 
-        return rates;
-    };
+    let filterNumbers = (filteredNumbers = numbers, bit = 0, mode = 'common') => {
+        let rate = rateAt(filteredNumbers, bit);
+        let bitValue = rate.one >= rate.zero ? 1 : 0;
+        if (mode != 'common') bitValue = Math.abs(bitValue - 1);
 
-    let filterOxygen = (filteredNumbers = numbers, bit = 0) => {
-        let rates = getRates(filteredNumbers);
-        let bitValue = rates[bit].one >= rates[bit].zero ? '1' : '0';
         let results = filteredNumbers.filter(x => x[bit] == bitValue);
         if (results.length == 1) return results[0];
 
-        return filterOxygen(results, bit + 1);
+        return filterNumbers(results, bit + 1, mode);
     };
 
-    let filterCO2 = (filteredNumbers = numbers, bit = 0) => {
-        let rates = getRates(filteredNumbers);
-        let bitValue = rates[bit].one >= rates[bit].zero ? '0' : '1';
-        let results = filteredNumbers.filter(x => x[bit] == bitValue);
-        if (results.length == 1) return results[0];
-
-        return filterCO2(results, bit + 1);
-    };
-
-    let oxygen2 = filterOxygen();
-    let co22 = filterCO2();
+    let oxygen2 = filterNumbers(numbers, 0, 'common');
+    let co22 = filterNumbers(numbers, 0, 'uncommon');
 
     let oxygen = parseInt(oxygen2, 2);
     let co2 = parseInt(co22, 2);
