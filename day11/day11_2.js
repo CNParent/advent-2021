cb6bcff9e1f1bfdbba1ca44f2fdd45b1ed15eeda
Chris Parent <chrisnparent@gmail.com>
2021-12-11 11:31:15 -0500

    day 11

---
+++ day11/day11_2.js
@@ -0,0 +1,58 @@
+scripts.day11_2 = async () => {
+    let data = await getFile('day11/input.txt');
+    let w = 10;
+    let h = 10;
+    let jellies = data.split('\r\n').map(x => x.split('').map(y => {
+        return { 
+            energy: Number(y),
+            flashed: false
+        };
+    }));
+
+    let flashes = 0;
+    let getEnergized = () => {
+        let energized = [];
+        for(let i = 0; i < h; i++) {
+            for(let j = 0; j < w; j++) {
+                if(jellies[i][j].energy > 9 && !jellies[i][j].flashed) 
+                    energized.push({ i, j });
+            }
+        }
+
+        return energized;
+    }
+
+    let step = () => {
+        jellies.reduce((a,b) => a.concat(b), []).forEach(x => x.energy++);
+
+        let energized = [];
+        do {
+            energized = getEnergized();
+            energized.forEach(x => flash(x.i, x.j));
+        } while(energized.length > 0)
+
+        let flashed = jellies.reduce((a,b) => a.concat(b), []).filter(x => x.flashed);
+        flashed.forEach(x => x.flashed = false);
+        flashed.forEach(x => x.energy = 0);
+        return flashed.length;
+    };
+
+    let flash = (i = 0, j = 0) => {
+        jellies[i][j].flashed = true;
+        for(let n = i-1; n <= i+1; n++) {
+            if (n < 0 || n >= h) continue;
+            for(let m = j-1; m <= j+1; m++) {
+                if (m < 0 || m >= h) continue;
+                if (n == i && m == j) continue;
+
+                jellies[n][m].energy++;
+            }
+        }
+    }
+    
+    let n = 1;
+    while(step() != h * w)
+        n++;
+
+    terminal.textContent = `The jellies synchronized on step ${n}!`;
+}
